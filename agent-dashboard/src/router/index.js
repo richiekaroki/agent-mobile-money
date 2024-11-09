2bc2d5459869f77039deb5cada250663f2856d8a
Richard Karoki <karokirichard522@gmail.com>
2024-11-09 20:21:20 +0300

    Create index.js

---
+++ agent-dashboard/src/router/index.js
@@ -0,0 +1,18 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import DashboardView from '../views/DashboardView.vue'
+import TransactionsView from '../views/TransactionsView.vue'
+import LoginUser from '@/views/LoginUser.vue'
+
+const routes = [
+  { path: '/', name: 'Dashboard', component: DashboardView },
+  { path: '/transactions', name: 'Transactions', component: TransactionsView },
+  {path: '/login', name: 'Login', component: LoginUser },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
