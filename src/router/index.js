f82056e7a341226ddcdeced9fdf091ec04e12c2d
Richard Karoki <138672297+richiekaroki@users.noreply.github.com>
2025-07-10 21:34:53 +0300

    Updated index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,23 @@
-@@ .. @@
- // src/router/index.js
- import { createRouter, createWebHistory } from 'vue-router'
- import DashboardView from '../views/DashboardView.vue'
- import AuthView from '../views/AuthView.vue'
- import TransactionsView from '../views/TransactionsView.vue'
-+import authService from '../services/authService'
- 
- // Auth guard
- const requireAuth = (to, from, next) => {
--  const token = localStorage.getItem('authToken')
--  if (token) {
-+  if (authService.isAuthenticated()) {
-     next()
-   } else {
-     next('/auth')
-@@ .. @@
- }
- 
- const redirectIfAuth = (to, from, next) => {
--  const token = localStorage.getItem('authToken')
--  if (token) {
-+  if (authService.isAuthenticated()) {
-     next('/')
-   } else {
-     next()
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import DashboardView from '../views/DashboardView.vue'
+import AuthView from '../views/AuthView.vue'
+import TransactionsView from '../views/TransactionsView.vue'
+import authService from '../services/authService'
+
+// Auth guard
+const requireAuth = (to, from, next) => {
+  if (authService.isAuthenticated()) {
+    next()
+  } else {
+    next('/auth')
+  }
+}
+
+const redirectIfAuth = (to, from, next) => {
+  if (authService.isAuthenticated()) {
+    next('/')
+  } else {
+    next()
+  }
+}
