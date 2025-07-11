7f483e80780476891de2df95c969faaabf50f3b0
Richard Karoki <karokirichard522@gmail.com>
2025-07-11 23:34:04 +0300

    Update NotificationComponent.vue

--- agent-dashboard/src/components/NotificationComponent.vue
+++ agent-dashboard/src/components/NotificationComponent.vue
@@ -15,34 +15,34 @@
         <div class="flex items-start">
           <!-- Icon -->
           <div class="flex-shrink-0 mr-3">
-            <svg 
-              v-if="notification.type === 'success'" 
-              class="w-5 h-5 text-green-600" 
-              fill="currentColor" 
+            <svg
+              v-if="notification.type === 'success'"
+              class="w-5 h-5 text-green-600"
+              fill="currentColor"
               viewBox="0 0 20 20"
             >
               <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
             </svg>
-            <svg 
-              v-else-if="notification.type === 'error'" 
-              class="w-5 h-5 text-red-600" 
-              fill="currentColor" 
+            <svg
+              v-else-if="notification.type === 'error'"
+              class="w-5 h-5 text-red-600"
+              fill="currentColor"
               viewBox="0 0 20 20"
             >
               <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd"></path>
             </svg>
-            <svg 
-              v-else-if="notification.type === 'warning'" 
-              class="w-5 h-5 text-yellow-600" 
-              fill="currentColor" 
+            <svg
+              v-else-if="notification.type === 'warning'"
+              class="w-5 h-5 text-yellow-600"
+              fill="currentColor"
               viewBox="0 0 20 20"
             >
               <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"></path>
             </svg>
-            <svg 
-              v-else 
-              class="w-5 h-5 text-blue-600" 
-              fill="currentColor" 
+            <svg
+              v-else
+              class="w-5 h-5 text-blue-600"
+              fill="currentColor"
               viewBox="0 0 20 20"
             >
               <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path>
@@ -60,8 +60,8 @@
           </div>
 
           <!-- Close button -->
-          <button 
-            @click="removeNotification(notification.id)" 
+          <button
+            @click="removeNotification(notification.id)"
             class="flex-shrink-0 ml-3 p-1 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-300 transition-colors duration-200"
             aria-label="Close notification"
           >
@@ -74,7 +74,7 @@
         <!-- Progress bar for auto-dismiss -->
         <div v-if="notification.autoDismiss" class="mt-3">
           <div class="w-full bg-gray-200 rounded-full h-1">
-            <div 
+            <div
               class="h-1 bg-gray-400 rounded-full transition-all duration-100 ease-linear"
               :style="{ width: `${getProgressWidth(notification)}%` }"
             ></div>
@@ -107,14 +107,14 @@ export default {
     const getNotificationClass = (type) => {
       const baseClasses = 'notification-card'
       switch (type) {
-        case 'success':
-          return `${baseClasses} notification-success`
-        case 'error':
-          return `${baseClasses} notification-error`
-        case 'warning':
-          return `${baseClasses} notification-warning`
-        default:
-          return `${baseClasses} notification-info`
+      case 'success':
+        return `${baseClasses} notification-success`
+      case 'error':
+        return `${baseClasses} notification-error`
+      case 'warning':
+        return `${baseClasses} notification-warning`
+      default:
+        return `${baseClasses} notification-info`
       }
     }
 
@@ -148,9 +148,9 @@ export default {
       progressIntervals.value.forEach(interval => clearInterval(interval))
     })
 
-    return { 
-      notifications, 
-      removeNotification, 
+    return {
+      notifications,
+      removeNotification,
       getNotificationClass,
       getProgressWidth
     }
